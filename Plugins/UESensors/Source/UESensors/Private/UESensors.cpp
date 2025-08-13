2c0337edda1c6b885ee0fa54fcf186f39827c386
Hanu <hanweonjin415@gmail.com>
2025-08-14 00:58:01 +0900

    Added UESensors plugin

---
+++ Plugins/UESensors/Source/UESensors/Private/UESensors.cpp
@@ -0,0 +1,15 @@
+#include "UESensors.h"
+
+#define LOCTEXT_NAMESPACE "FUESensorsModule"
+
+void FUESensorsModule::StartupModule()
+{
+}
+
+void FUESensorsModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUESensorsModule, UESensors)
---
+++ Plugins/UESensors/Source/UESensors/UESensors.Build.cs
@@ -0,0 +1,16 @@
+using UnrealBuildTool;
+
+public class UESensors : ModuleRules
+{
+	public UESensors(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core", "CoreUObject", "Engine"
+			}
+		);
+	}
+}
