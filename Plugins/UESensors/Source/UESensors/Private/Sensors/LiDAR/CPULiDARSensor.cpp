e0ac9bcc1b1f7ea110ca108b2ab7604895ce0558
Hanu <hanweonjin415@gmail.com>
2025-08-20 00:56:33 +0900

    Setup basic framework for LiDAR sensor

---
+++ Plugins/UESensors/Source/UESensors/Private/Sensors/LiDAR/CPULiDARSensor.cpp
@@ -0,0 +1,28 @@
+﻿#include "Sensors/LiDAR/CPULiDARSensor.h"
+
+UCPULiDARSensor::UCPULiDARSensor()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UCPULiDARSensor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// TEMPORARY: debug draw all laser directions
+	if (const UWorld* World{ GetWorld() })
+	{
+		for (const FVector& LaserDirection : LaserDirections)
+		{
+			const FVector LaserStart{ GetComponentLocation() };
+			const FVector LaserEnd{
+				LaserStart + GetComponentRotation().RotateVector(LaserDirection) * DebugDrawDistance
+			};
+		
+			DrawDebugLine(
+				World, LaserStart, LaserEnd,
+				FColor::Red, false, -1.0F, 0U, 0.0F
+			);
+		}
+	}
+}
---
+++ Plugins/UESensors/Source/UESensors/Public/Sensors/LiDAR/CPULiDARSensor.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseLiDARSensor.h"
+#include "CPULiDARSensor.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class UESENSORS_API UCPULiDARSensor : public UBaseLiDARSensor
+{
+	GENERATED_BODY()
+
+public:
+	UCPULiDARSensor();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DebugDrawDistance{ 1000.0F };
+};
